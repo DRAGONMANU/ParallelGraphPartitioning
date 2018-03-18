765860e6bbcd39d17bc3b6fbab55f457a780c0ca
DRAGONMANU <manumitraan@gmail.com>
2018-03-18 16:01:12 +0530

    P: Printing Graph done

--- Graph.cpp
+++ Graph.cpp
@@ -12,13 +12,31 @@
 
 using namespace std;
 
+class Edge
+{
+public:
+	int n2;
+	int weight;
+	
+	Edge(int a2, int wt) {
+		n2 = a2;
+		weight = wt;
+	}
+
+	void printEdge()
+	{
+		printf("[ %d (%d) ]", n2, weight);
+	}
+};
+
 class Node
 {
 	public:	
 	int id;
 	int weight;
 	int matched;
-	struct Node* eaten;
+	Node* eaten;
+	vector<Edge> external_edges;
 	Node(){
 		id = -1;
 		matched = 0;
@@ -38,24 +56,24 @@ class Node
 	}
 };
 
-
-
-class Edge
-{
-public:
-	int n2;
-	int weight;
-	Edge(int a2, int wt) {
-		n2 = a2;
-		weight = wt;
-	}
-};
-
 class Graph
 {
 public:
 	vector <tuple<Node,vector <Edge>>> adjacency_list;
 	Graph(){}
+	
+	int NodeExists(int x)
+	{
+		for (unsigned int i = 0; i < adjacency_list.size(); i++)
+		{
+			if (x == get<0>(adjacency_list[i]).getId())
+			{
+				return 1;
+			}
+		}
+		return 0;
+	}
+
 	Node& getNode(int x)
 	{
 		Node n =(*new Node());
@@ -71,8 +89,30 @@ public:
 
 	void createAdjacencyList(int x1, vector<Edge> neighbours)
 	{
-		Node n1 = getNode(x1);
+		Node n1;
+		if (NodeExists(x1) != 0)
+		{
+		 	n1 = getNode(x1);
+		}
+		else
+		{
+			n1 = *(new Node(x1));
+		}
 		adjacency_list.push_back( make_tuple(n1, neighbours));
+	}
+
+	void printGraph() 
+	{
+		printf("Printing Graph\n");
+		for (unsigned int i = 0; i < adjacency_list.size(); i++)
+		{
+			printf("Node- %d ", get<0>(adjacency_list[i]).getId());
+			for (unsigned int j = 0; j < get<1>(adjacency_list[i]).size(); j++ )
+			{
+				( get<1>(adjacency_list[i])[j] ).printEdge();
+			}
+			printf("\n");
+		}
 	}
 };
 
